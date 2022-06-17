a94edf4bd16d8bc2524ea9be78037d5d15dab651
Anel Dominguez <anelgabriel002@gmail.com>
2022-06-17 16:18:29 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import NewsLetter from "./components/NewsLetter";
+
+function App() {
+  return (
+    <div >
+      <NavBar />
+      <Hero />
+      <Analytics />
+      <NewsLetter />
+      <Cards />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
